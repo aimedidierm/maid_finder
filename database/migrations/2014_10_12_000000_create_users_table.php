69654bcf4abd29f1a52a08540715a8bb1548de12
aimedidierm <aimedidiermugisha@gmail.com>
2023-07-16 02:43:02 +0200

    Tailwind CSS config

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('address');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['admin', 'employer'])->default('employer');
             $table->timestamps();
         });
     }
