be966dcf4d1d4aa9777e26b1d64072203b7a39e4
aimedidierm <aimedidiermugisha@gmail.com>
2023-07-30 03:24:43 +0200

    Sidebar fix

--- resources/views/admin/contracts.blade.php
+++ resources/views/admin/contracts.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-<x-employer-nav />
+<x-admin-nav />
 <div class="p-4 sm:ml-64">
     <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">All maid contracts</h5>
